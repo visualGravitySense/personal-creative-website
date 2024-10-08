35041c0e935e1bf445052d0e7e8c465fd2a191ca
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-10-08 13:55:48 +0300

    new model : onlice claseees and freelance community

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import AdminDashboard from '../components/AdminDashboard';
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import FeaturedCourses from "../components/FeaturedCourses";
+import CourseCategories from "../components/CourseCategories";
+import PopularCourses from "../components/PopularCourses";
+import PromoBanners from "../components/PromoBanners";
+import AboutSection from "../components/AboutSection";
+import StudentTestimonials from "../components/StudentTestimonials";
+import "./Home.scss";
 
-const Profile = () => {
+const posts = [
+  {
+    id: "uiux-ruin-product",
+    title: "UI/UX Design Can Actually Ruin a Product",
+  },
+];
+
+const Profile = ({ theme, toggleTheme }) => {
   return (
-    <div>
-      {/* <h2>User Profile</h2>
-      <p>Here you can edit your personal information, see your course progress, and manage your purchased courses.</p> */}
-      <AdminDashboard/>
-      {/* Добавьте элементы управления и отображения прогресса */}
+    <div className={`home-page ${theme === "dark" ? "dark-mode" : ""}`}>
+      <HeroSection theme={theme} toggleTheme={toggleTheme} />
+      <AboutSection theme={theme} />
+      <PromoBanners theme={theme} />
+      <PopularCourses theme={theme} />
+      {/* <StudentTestimonials theme={theme} /> */}
+      {/* <FeaturedCourses theme={theme} toggleTheme={toggleTheme} /> */}
+      {/* <CourseCategories theme={theme} /> */}
+      {/* Promo blocks, banners, platform info, etc. */}
+
+      
     </div>
   );
 };
